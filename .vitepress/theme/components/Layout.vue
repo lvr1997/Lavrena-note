be366109a6da2b4c6d1a67efb492f48865653286
Riny <37863122+lvr1997@users.noreply.github.com>
2026-01-06 15:13:05 +0800

    Rename layout-after template to nav-bar-title-after

--- .vitepress/theme/components/Layout.vue
+++ .vitepress/theme/components/Layout.vue
@@ -43,7 +43,7 @@ onMounted(() => {
         <path d="M4 19.5v-15A2.5 2.5 0 0 1 6.5 2H20v20H6.5a2.5 2.5 0 0 1 0-5H20" />
       </svg>
     </template>
-    <template #layout-after>
+    <template #nav-bar-title-after>
       <Visitor />
     </template>
     <template #doc-before>
