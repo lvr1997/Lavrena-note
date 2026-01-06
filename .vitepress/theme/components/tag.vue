45e0fbb10da28f7c2d42cacc41063ddc40c2fdd1
Riny <37863122+lvr1997@users.noreply.github.com>
2026-01-06 16:04:13 +0800

    Update date display in tag component
    Replaced formatted date string with direct frontmatter date display.

--- .vitepress/theme/components/tag.vue
+++ .vitepress/theme/components/tag.vue
@@ -2,9 +2,6 @@
 import { useData } from "vitepress";
 
 const { frontmatter } = useData()
-const date = new Date(frontmatter.value.date)
-
-const dateStr = `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(2, '0')}-${String(date.getDate()).padStart(2, '0')} ${String(date.getHours()).padStart(2, '0')}:${String(date.getMinutes()).padStart(2, '0')}:${String(date.getSeconds()).padStart(2, '0')}`
 
 </script>
 <template>
@@ -12,7 +9,7 @@ const dateStr = `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(2,
         <!-- <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 32 32" class="xicon-icon"><path
                 d="M26 4h-4V2h-2v2h-8V2h-2v2H6c-1.1 0-2 .9-2 2v20c0 1.1.9 2 2 2h20c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 22H6V12h20v14zm0-16H6V6h4v2h2V6h8v2h2V6h4v4z"
                 fill="currentColor"></path></svg> -->
-        {{ dateStr }}
+        {{ frontmatter.date }}
     </span>
 </template>
 <style>
